4fe7399601d239832f75f15463ea96f1f970f7be
thatmose <moseswanyonyi@gmail.com>
2016-07-11 15:15:35 -0600

    Messages can be added. Validations upto part C

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,25 @@ get '/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
